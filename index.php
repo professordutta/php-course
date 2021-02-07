eb2d74739ce418ac04b244cf3adab038f3410bb3
professordutta <umeshdutta.fet@mriu.edu.in>
2021-02-07 09:31:45 +0530

    add tax calc function

--- index.php
+++ index.php
@@ -26,6 +26,15 @@
           echo "<p>".$key."</p>"; 
         }
       }
+      
+      echo tax_calc(750,0.223);
+      
+      function tax_calc($amount,$tax){
+        $calculate_tax = $amount*$tax;
+        $amount = round($amount+$calculate_tax,2);
+        return $amount;
+      }
+
     ?>
   </body>
 </html>
